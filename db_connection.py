d3f2532264fb56752a60400da6718ba463e9d3cd
Sayem Mohammad Nadim <89217335+Nadim26@users.noreply.github.com>
2021-11-02 22:00:20 +0600

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 
 with open('credintials.yml', 'r') as f:
-    credintials = yaml.safe_load(f, Loader=yaml.FullLoader)
+    credintials = yaml.load(f, Loader=yaml.FullLoader)
     db_credintials = credintials['db']
     system_pass = credintials['system_pass']['admin']
     # email_sender = credintials['email_sender']
